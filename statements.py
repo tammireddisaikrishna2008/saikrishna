ba9c91448622d17a1501b19fec6971ccdc399061
tammireddisaikrishna2008 <tammmireddisaikrishna@gmail.com>
2026-07-27 12:43:14 +0530

    statements.py

---
+++ statements.py
@@ -0,0 +1,42 @@
+#Name: T.sai krishna
+#Lab:04
+#Task:01
+#Program:Python Statements
+#print statement
+print("I'm a CSE student")
+#assignment statement
+age=18
+#if statement
+if 7>5:
+    print("7 is greater than 5!!")
+chocolates=["Dairy Milk","Five Star","Kit Kat"]
+#for statement
+for x in chocolates:
+    print(x)
+    #Name: T.Saikrishna
+#Lab:04
+#Task:02
+#Program:Multiline Statements
+#using backslash(\)
+sum=10+20+\
+57+64
+print(sum)
+#using parantheses('()')
+total=(47+43+
+       27+57)
+print(total)
+#Name: T.Sai krishna
+#Lab:04
+#Task:03
+#Program:Single line with multiple statements
+a=10;b=15
+print("I'm a good boy");name=input("What's your name?");print("oh so you're ",name)
+#Name: T.Sai krishna
+#Lab:04
+#Task:Challenge
+#A Statemet is an instruction that Python executes
+x=10
+#An expression is a combination of values,variables, and operators tha produces a value
+y=x+5
+print(x)#statement
+print(y)#statement 
