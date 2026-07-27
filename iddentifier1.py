6032eac4f3fb648190585fea17ede0c082ae402f
tammireddisaikrishna2008 <tammmireddisaikrishna@gmail.com>
2026-07-27 11:53:54 +0530

    Add initial implementation of identifier validation and case sensitivity demonstration

---
+++ iddentifier1.py
@@ -0,0 +1,23 @@
+#Name:T.sai krishna
+#Lab:01
+#Task:01
+#Program:Valid Identifiers
+name="sai krishna"
+PI=3.14
+def greet():
+    print("All the best")
+class Add:
+    pass
+jersey_number=45
+print(name)
+print(PI)
+print(greet)
+print(Add)
+print(jersey_number)
+#Lab:01
+#Task:03
+#Program:Case Sensitivity of Identifiers
+Marks=45
+marks=18
+print(Marks)
+print(marks)
