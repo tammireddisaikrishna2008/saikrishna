65f4268b61c56b1e6a306c444c561dcfdcc37279
tammireddisaikrishna2008 <tammmireddisaikrishna@gmail.com>
2026-08-03 21:44:01 +0530

    Add assignment operators program

---
+++ datatypes/assignment.py
@@ -0,0 +1,29 @@
+#Name: T.Sai krishna
+#Program:Assignment Operators
+#Task:B3.1-Assignment Operators
+score=50
+a=5
+print("Score=",score)
+score+=a
+print("score+=a is",score)
+score-=a
+print("score-=a is",score)
+score*=a
+print("score*=a is",score)
+score/=a
+print("score/=a is",score)
+score//=a
+print("score//=a is",score)
+score%=a
+print("score%=a is",score)
+score**=a
+print("score**=a is",score)
+#output
+#Score= 50
+#score+=a is 55
+#score-=a is 50
+#score*=a is 250
+#score/=a is 50.0
+#score//=a is 10.0
+#score%=a is 0.0
+#score**=a is 0.0
