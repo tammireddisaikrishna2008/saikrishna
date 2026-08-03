07be2bce84f4379762a83dadf10934d39b47e5b4
tammireddisaikrishna2008 <tammmireddisaikrishna@gmail.com>
2026-08-03 22:20:25 +0530

    Add logical.py

---
+++ operations/logical.py
@@ -0,0 +1,15 @@
+#Name: T.Sai krishna
+#Program:Scholarship_Eligibility
+#Task:B4.1-Logical Operators
+percentage=float(input("Enter your Percentage\n"))
+attendence=float(input("Enter your Attendence\n"))
+if percentage>75 and attendence>90:
+    print("You are eligile for scholarship")
+else:
+    print("You are not eligible for scholarship")
+#output
+#Enter your Percentage
+#95
+#Enter your Attendence
+#98
+#You are eligile for scholarship
