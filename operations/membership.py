a373cadff4963edb88eb5a556515e8b9803028f2
tammireddisaikrishna2008 <tammmireddisaikrishna@gmail.com>
2026-08-03 22:18:30 +0530

    Add membership.py

---
+++ operations/membership.py
@@ -0,0 +1,19 @@
+#Name: T.Sai krishna
+#Program:Fruit Membership Check
+#Task:B6.1-Membership Operators
+fruits=["kiwi","guava","dragon fruit","avacado"]
+fruit1=input("Enter any fruit name\n")
+print(f'{fruit1} in fruits ??',fruit1 in fruits)
+print(f'{fruit1} not in fruits ??',fruit1 not in fruits)
+fruit2=input("Enter any fruit name again\n")
+print(f'{fruit2} in fruits ??',fruit2 in fruits)
+print(f'{fruit2} not in fruits ??',fruit2 not in fruits)
+#output
+#Enter any fruit name
+#mango
+#mango in fruits ?? False
+#mango not in fruits ?? True
+#Enter any fruit name again
+#kiwi
+#kiwi in fruits ?? True
+#kiwi not in fruits ?? False
