85c893f48123c6d3380940a1aed7451121f47d23
tammireddisaikrishna2008 <tammmireddisaikrishna@gmail.com>
2026-08-03 21:46:14 +0530

    Add comparsion operators program

---
+++ datatypes/comparsion.py
@@ -0,0 +1,19 @@
+#Name: T.Sai krishna
+#Program:Comparision Operators
+#Task:B2.1-Comparision Operators
+m,n=map(int,input("Enter m,n values\n").split())
+print("m == n is",m==n)
+print("m!=n is",m!=n)
+print("m>n is",m>n)
+print("m<n is",m<n)
+print("m>=n is",m>=n)
+print("m<=n is",m<=n)
+#output
+#Enter m,n values
+#5 11
+#m == n is False
+#m!=n is True
+#m>n is False
+#m<n is True
+#m>=n is False
+#m<=n is True
