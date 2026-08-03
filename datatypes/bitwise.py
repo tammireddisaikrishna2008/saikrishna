58160a16836c2b0363bb40858e758cbba7e428a8
tammireddisaikrishna2008 <tammmireddisaikrishna@gmail.com>
2026-08-03 21:48:20 +0530

    Add bitwise operators program

---
+++ datatypes/bitwise.py
@@ -0,0 +1,17 @@
+#Name: T.Sai krishna
+#Program:Bitwise Operators
+#Task:B5.1-Bitwise Operators
+p=12;q=10
+print("p&q is:",p&q)
+print("p^q is:",p^q)
+print("p|q is:",p|q)
+print("~p is:",~p)
+print("p>>2 is:",p>>2)
+print("p<<2 is:",p<<2)
+#output
+#p&q is: 8
+#p^q is: 6
+#p|q is: 14
+#~p is: -13
+#p>>2 is: 3
+#p<<2 is: 48
