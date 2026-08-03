9a16046a4c257405aef33755717e64e419ee74bc
tammireddisaikrishna2008 <tammmireddisaikrishna@gmail.com>
2026-08-03 21:41:05 +0530

    Add arithmetic operators program

---
+++ datatypes/arithmetic.py
@@ -0,0 +1,21 @@
+#Name: T.Sai krishna
+
+#Program:Arithmetic Operators
+#Task:B1.1-Arithmetic Operators
+a=23
+b=6
+print("Addition is :",a+b)
+print("Subtarction is :",a-b)
+print("Multiplication is:",a*b)
+print("Division is :",a/b)
+print("Floor division is:",a//b)
+print("Modulus is :",a%b)
+print("Exponent is:",a**b)
+#output
+#Addition is : 29
+#Subtarction is : 17
+#Multiplication is: 138
+#Division is : 3.8333333333333335
+#Floor division is: 3
+#Modulus is : 5
+#Exponent is: 148035889
