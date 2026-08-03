e52ef652199e92e7dcb0727778baf8494dd98532
tammireddisaikrishna2008 <tammmireddisaikrishna@gmail.com>
2026-08-03 22:05:20 +0530

    Add integers.py

---
+++ datatypes/integers.py
@@ -0,0 +1,16 @@
+#Name: T.Sai krishna
+#Program:Integer Operations
+#Task:A1.2-Integer Division,Modulus and Exponent
+x=int(input("Enter x value\n"))
+y=int(input("Enter y value\n"))
+print("Integer Division of x and y is",x//y)
+print("Exponent of x and y is",x**y)
+print("Modulus of x and y is",x%y)
+#output
+#Enter x value
+#4
+#Enter y value
+#2
+#Integer Division of x and y is 2
+#Exponent of x and y is 16
+#Modulus of x and y is 0
